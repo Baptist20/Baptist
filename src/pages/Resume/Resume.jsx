60f877344a7bca2410985d2287abe60332d9511b
Ezimah Baptist Ikenna <baptistcontactsgitg@gmail.com>
2025-04-12 10:31:30 +0100

    Update Resume.jsx

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -48,17 +48,24 @@ function Resume() {
             title="HTML, CSS and JavaScript"
             date="2022 — Present"
           />
-          <TimelineItem title="React" date="2023 — present" />
+          <TimelineItem title="React + Vite" date="2023 — present" />
+          <TimelineItem title="Next" date="2024 — present" />
+          <TimelineItem title="Node, Express, MongoDB" date="2025 — present" />
         </ol>
       </div>
 
       <div className="skill">
         <h3 className="h3 skills-title">My skills</h3>
         <ul className="skills-list content-card">
-          <SkillItem title="HTML" value={80} />
-          <SkillItem title="CSS" value={90} />
+          <SkillItem title="HTML" value={95} />
+          <SkillItem title="CSS" value={75} />
           <SkillItem title="Javascript" value={90} />
-          <SkillItem title="React" value={70} />
+          <SkillItem title="React" value={90} />
+          <SkillItem title="Tailwindcss" value={80} />
+          <SkillItem title="Vite" value={90} />
+          <SkillItem title="Next.js" value={90} />
+          <SkillItem title="Express" value={90} />
+          <SkillItem title="MongoDB" value={90} />
         </ul>
       </div>
     </section>
